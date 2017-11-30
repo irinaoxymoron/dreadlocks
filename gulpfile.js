a153b59771d3b47b451a8550d9a44fb99ef3d026
irinaoxymoron <irinaoxymoron@gmail.com>
2017-11-30 18:17:38 +0300

    gulpfile was updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,317 @@
 'use strict';
-
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    newer = require('gulp-newer'),
-    csso = require('gulp-csso'), // минификатор CSS
-    cleanCSS = require('gulp-clean-css'),
-    postcss = require('gulp-postcss'), // npm install --save-dev gulp-postcss
-    autoprefixer = require('autoprefixer'), // npm install autoprefixer --save-dev
-    watch = require('gulp-watch'),
-    gulpif = require('gulp-if'),
-    uglify = require('gulp-uglify'),
-    browserify = require('gulp-browserify'),
-    babel = require('gulp-babel'),
-    fileinclude = require('gulp-file-include'),
-    browserSync = require('browser-sync').create(),
-    argv = require('yargs').argv,
-    spritesmith = require('gulp.spritesmith'),
-    prod = argv.prod; // минификация
-
-
-var app_dir = 'app';
-var wp_dir = 'dist';
-
-
-gulp.task('browser-sync', function () {
-    browserSync.init({
-        server: {
-            baseDir: wp_dir
-        }
-    });
-});
-
-gulp.task('sass', function () {
-    return gulp.src(app_dir + '/scss/**/*.scss')
-        .pipe(gulpif(!Boolean(prod), sourcemaps.init()))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([autoprefixer({browsers: ['last 5 versions']})]))
-        .pipe(gulpif(Boolean(prod), csso()))
+const pjson = require('./package.json');
+const dirs = pjson.config.directories;
+const gulp = require('gulp');
+const less = require('gulp-less');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const replace = require('gulp-replace');
+const fileinclude = require('gulp-file-include');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const ghPages = require('gulp-gh-pages');
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const pngquant = require('imagemin-pngquant');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const cheerio = require('gulp-cheerio');
+const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
+const base64 = require('gulp-base64');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const cleanCSS = require('gulp-cleancss');
+const pug = require('gulp-pug');
+const spritesmith = require('gulp.spritesmith');
+const buffer = require('vinyl-buffer');
+const merge = require('merge-stream');
+// Компиляция препроцессора
+gulp.task('less', function(){
+    return gulp.src(dirs.source + '/less/style.less')
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(postcss([
+            autoprefixer({ browsers: ['last 2 version'] }),
+            mqpacker({ sort: true }),
+        ]))
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(dirs.build + '/css/'))
+        .pipe(browserSync.stream())
+        .pipe(rename('style.min.css'))
         .pipe(cleanCSS())
-        .pipe(gulpif(!Boolean(prod), sourcemaps.write()))
-        .pipe(gulp.dest(wp_dir + '/css'));
+        .pipe(gulp.dest(dirs.build + '/css/'));
 });
-
-gulp.task('moveJs', function () {
-    return gulp.src(app_dir + '/js/*.js')
-        .pipe(gulpif(Boolean(prod), babel({
-            presets: ['es2015']
-        })))
-        .pipe(gulpif(Boolean(prod), uglify()))
-        .pipe(gulp.dest(wp_dir + '/js'));
+// Сборка HTML
+gulp.task('html', function() {
+    return gulp.src(dirs.source + '/*.html')
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file',
+            indent: true,
+        }))
+        .pipe(replace(/\n\s*<!--DEV[\s\S]+?-->/gm, ''))
+        .pipe(gulp.dest(dirs.build));
 });
-
-/*   pipe(pagebuilder(app_dir))         */
-gulp.task('moveHtml', function () {
-    return gulp.src(app_dir + '/*.html')
-        .pipe(fileinclude())
-        .pipe(gulp.dest(wp_dir));
+gulp.task('pug', function() {
+    return gulp.src(dirs.source + '/**/*.pug')
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(pug())
+        .pipe(gulp.dest(dirs.build));
 });
-
-/*   Sprite   */
-gulp.task('sprite', function(cb) {
-    const spriteData = gulp.src('app/img/icons/*.png').pipe(spritesmith({
-        imgName: 'sprite.png',
-        imgPath: '../../img/sprite.png',
-        cssName: '_sprite.scss'
-    }));
-
-    spriteData.img.pipe(gulp.dest('app/img/'));
-    spriteData.css.pipe(gulp.dest('app/scss/modules/'));
-    cb();
+// ЗАДАЧА: Копирование изображений
+gulp.task('img', function () {
+    return gulp.src([
+            dirs.source + '/img/*.{gif,png,jpg,jpeg,svg}',
+        ],
+        {since: gulp.lastRun('img')}
+        )
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(newer(dirs.build + '/img'))
+        .pipe(gulp.dest(dirs.build + '/img'));
 });
-
-gulp.task('moveImg', function () {
-    return gulp.src(app_dir + '/img/**.*')
-        .pipe(gulp.dest(wp_dir + '/img'));
+// Оптимизация изображений (ВРУЧНУЮ)
+gulp.task('img:opt', function () {
+    return gulp.src([
+            dirs.source + '/img/*.{gif,png,jpg,jpeg,svg}',
+            '!' + dirs.source + '/img/sprite-svg.svg',
+        ])
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 7}),
+            imagemin.svgo({plugins: [{removeViewBox: false}]})
+        ]))
+        .pipe(gulp.dest(dirs.source + '/img'));
 });
-
-gulp.task('moveFonts', function () {
-    return gulp.src(app_dir + '/fonts/**.*')
-        .pipe(gulp.dest(wp_dir + '/fonts'));
+// ЗАДАЧА: Сборка SVG-спрайта
+gulp.task('svgstore', function (callback) {
+    let spritePath = dirs.source + '/img/svg-sprite';
+    if(fileExist(spritePath) !== false) {
+        return gulp.src(spritePath + '/*.svg')
+            // .pipe(plumber({ errorHandler: onError }))
+            .pipe(svgmin(function (file) {
+                return {
+                    plugins: [{
+                        cleanupIDs: {
+                            minify: true
+                        }
+                    }]
+                }
+            }))
+            .pipe(svgstore({ inlineSvg: true }))
+            .pipe(cheerio(function ($) {
+                $('svg').attr('style',  'display:none');
+            }))
+            .pipe(rename('sprite-svg.svg'))
+            .pipe(gulp.dest(dirs.source + '/img'));
+    }
+    else {
+        console.log('Нет файлов для сборки SVG-спрайта');
+        callback();
+    }
 });
-
-
-gulp.task('watch', function () {
-    gulp.watch(app_dir + '/scss/**/*.scss', gulp.series('sass'));
-    gulp.watch(app_dir + '/js/*.js', gulp.series('moveJs'));
-    gulp.watch(app_dir + '/**/*.html', gulp.series('moveHtml'));
-    gulp.watch(app_dir + '/img/*.*', gulp.series('moveImg'));
-    // gulp.watch(app_dir + '/**/*').on('change', browserSync.reload);
+// Cшивка PNG-спрайта
+gulp.task('png:sprite', function () {
+    let fileName = 'sprite-' + Math.random().toString().replace(/[^0-9]/g, '') + '.png';
+    let spriteData = gulp.src('src/img/png-sprite/*.png')
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(spritesmith({
+            imgName: fileName,
+            cssName: 'sprite.less',
+            cssFormat: 'less',
+            padding: 4,
+            imgPath: '../img/' + fileName
+        }));
+    let imgStream = spriteData.img
+        .pipe(buffer())
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img'));
+    let cssStream = spriteData.css
+        .pipe(gulp.dest(dirs.source + '/less/'));
+    return merge(imgStream, cssStream);
 });
-
-gulp.task('rebase', gulp.series(
-    'moveHtml',
-    'moveImg',
-    'moveJs',
-    'sass',
-    'moveFonts'
+// Очистка папки сборки
+gulp.task('clean', function () {
+    return del([
+        dirs.build + '/**/*',
+        '!' + dirs.build + '/readme.md'
+    ]);
+});
+// Конкатенация и углификация Javascript
+gulp.task('js', function () {
+    return gulp.src([
+            dirs.source + '/js/jquery-3.1.0.min.js',
+            dirs.source + '/js/jquery-migrate-1.4.1.min.js',
+            dirs.source + '/js/jquery.validate.js',
+            dirs.source + '/js/validate.messages.js',
+            dirs.source + '/js/jquery.fancybox.min.js',
+            dirs.source + '/js/url-polyfill.js',
+            dirs.source + '/js/picturefill.min.js',
+            dirs.source + '/js/owl.carousel.min.js',
+            dirs.source + '/js/datepicker.min.js',
+            dirs.source + '/js/select2.min.js',
+            dirs.source + '/js/inputmask.js',
+            dirs.source + '/js/jquery.inputmask.js',
+            dirs.source + '/js/inputmask.phone.extensions.js',
+            dirs.source + '/js/inputmask.date.extensions.js',
+            dirs.source + '/js/phone-ru.js',
+            dirs.source + '/js/social-likes.min.js',
+            dirs.source + '/js/jquery.mCustomScrollbar.concat.min.js',
+            dirs.source + '/js/svg-sprite.js',
+            dirs.source + '/js/mail.js',
+            dirs.source + '/js/piano.js',
+            dirs.source + '/js/script.js'
+        ])
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dirs.build + '/js'));
+});
+//Копирование шрифтов
+gulp.task('fonts', function () {
+    return gulp.src([
+            dirs.source + '/fonts/*.woff',
+            dirs.source + '/fonts/*.woff2'
+        ])
+        .pipe(gulp.dest(dirs.build + '/fonts'));
+});
+// Копирование сэмплов
+gulp.task('samples', function () {
+    return gulp.src([
+            dirs.source + '/samples/*.mp3'
+        ])
+        .pipe(gulp.dest(dirs.build + '/samples'));
+});
+gulp.task('copy-js', function () {
+    return gulp.src([
+            dirs.source + '/js/piano.js'
+        ])
+        .pipe(gulp.dest(dirs.build + '/js'));
+});
+// Кодирование в base64 шрифта в формате WOFF
+gulp.task('css:fonts:woff', function (callback) {
+    let fontCssPath = dirs.source + '/fonts/font_opensans_woff.css';
+    if(fileExist(fontCssPath) !== false) {
+        return gulp.src(fontCssPath)
+            .pipe(plumber({ errorHandler: onError }))
+            .pipe(base64({
+                // baseDir: '/',
+                extensions: ['woff'],
+                maxImageSize: 1024*1024,
+                deleteAfterEncoding: false,
+                // debug: true
+            }))
+            .pipe(gulp.dest(dirs.build + '/css'));
+    }
+    else {
+        console.log('Файла WOFF, из которого генерируется CSS с base64-кодированным шрифтом, нет');
+        console.log('Отсутствующий файл: ' + fontCssPath);
+        callback();
+    }
+});
+// Кодирование в base64 шрифта в формате WOFF2
+gulp.task('css:fonts:woff2', function (callback) {
+    let fontCssPath = dirs.source + '/fonts/font_opensans_woff2.css';
+    if(fileExist(fontCssPath) !== false) {
+        return gulp.src(fontCssPath)
+            .pipe(plumber({ errorHandler: onError }))
+            .pipe(base64({
+                // baseDir: '/',
+                extensions: ['woff2'],
+                maxImageSize: 1024*1024,
+                deleteAfterEncoding: false,
+                // debug: true
+            }))
+            .pipe(replace('application/octet-stream;', 'application/font-woff2;'))
+            .pipe(gulp.dest(dirs.build + '/css'));
+    }
+    else {
+        console.log('Файла WOFF2, из которого генерируется CSS с base64-кодированным шрифтом, нет');
+        console.log('Отсутствующий файл: ' + fontCssPath);
+        callback();
+    }
+});
+// Сборка
+gulp.task('build', gulp.series(
+    'clean',
+    'svgstore',
+    'png:sprite',
+    gulp.parallel('less', 'img', 'js', 'fonts', 'samples'),
+    'pug',
+    'html'
 ));
-
-gulp.task('default', gulp.series('rebase', gulp.parallel('browser-sync', 'watch')));
+// Локальный сервер, слежение
+gulp.task('serve', gulp.series('build', function() {
+    browserSync.init({
+        //server: dirs.build,
+        server: {
+            baseDir: "./build/"
+        },
+        port: 3000,
+        startPath: '/index.html',
+        // open: false
+    });
+    gulp.watch(
+        [
+            dirs.source + '/*.html',
+            dirs.source + '/_include/*.html',
+        ],
+        gulp.series('html', reloader)
+    );
+    gulp.watch(
+        dirs.source + '/pug/**/*.pug',
+        gulp.series('pug', reloader)
+    );
+    gulp.watch(
+        dirs.source + '/less/**/*.less',
+        gulp.series('less')
+    );
+    gulp.watch(
+        dirs.source + '/img/svg-sprite/*.svg',
+        gulp.series('svgstore', 'html', 'pug', reloader)
+    );
+    gulp.watch(
+        dirs.source + '/img/png-sprite/*.png',
+        gulp.series('png:sprite', 'less')
+    );
+    gulp.watch(
+        dirs.source + '/img/*.{gif,png,jpg,jpeg,svg}',
+        gulp.series('img', reloader)
+    );
+    gulp.watch(
+        dirs.source + '/js/*.js',
+        gulp.series('js', reloader)
+    );
+}));
+// Задача по умолчанию
+gulp.task('default',
+    gulp.series('serve')
+);
+// Дополнительная функция для перезагрузки в браузере
+function reloader(done) {
+    browserSync.reload();
+    done();
+}
+// Проверка существования файла/папки
+function fileExist(path) {
+    const fs = require('fs');
+    try {
+        fs.statSync(path);
+    } catch(err) {
+        return !(err && err.code === 'ENOENT');
+    }
+}
+var onError = function(err) {
+    notify.onError({
+        title: "Error in " + err.plugin,
+    })(err);
+    this.emit('end');
+};
